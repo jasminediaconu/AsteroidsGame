1ba1282806fefbf922b9289f7cea1cfc4294f133
Jasmine Diaconu <j.c.diaconu@student.tudelft.nl>
2020-01-07 15:48:47 +0100

    Implemented top 5 scores on the LeaderBoard screen + tests

---
+++ src/main/java/game/LeaderBoardGame.java
@@ -0,0 +1,137 @@
+package game;
+
+import java.sql.Date;
+import java.util.Objects;
+
+/**
+ * The type User.
+ */
+public class LeaderBoardGame {
+
+    /**
+     * The username or alias of the player.
+     */
+    protected String username;
+
+    /**
+     * The score.
+     */
+    protected int score;
+
+    /**
+     * The date.
+     */
+    protected transient Date date;
+
+    /**
+     * The abstract User constructor, used for all other users that are not the client.
+     */
+    public LeaderBoardGame() {
+    }
+
+    /**
+     * Instantiates a new LeaderBoardGame.
+     *
+     * @param username the username or alias
+     * @param score the score
+     * @param date the date
+     */
+    public LeaderBoardGame(String username, int score, Date date) {
+        this.username = username;
+        this.score = score;
+        this.date = date;
+    }
+
+    /**
+     * This function will get the players username or alias.
+     *
+     * @return the username or alias of the player
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets the username.
+     *
+     * @param username the username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * This function will get the users score.
+     *
+     * @return the score of the user
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * Sets score.
+     *
+     * @param score the score
+     */
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    /**
+     * This function will get the game date.
+     *
+     * @return returns the game date
+     */
+    public Date getTimestamp() {
+        return date;
+    }
+
+    /**
+     * This function will set the date of the game.
+     *
+     * @param date the date
+     */
+    public void setTimestamp(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "LeaderBoardGame{"
+                + "username=" + username
+                + ", score=" + score
+                + ", date=" + date
+                + '}';
+    }
+
+    /**
+     * This function compares this Game with another Game to check if they are equal.
+     *
+     * @param obj Object type
+     * @return a boolean, whether they are equal or not
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        LeaderBoardGame game = (LeaderBoardGame) obj;
+        return Double.compare(game.getScore(), score) == 0
+                && Objects.equals(game.getUsername(), username)
+                && Objects.equals(game.getTimestamp(), date);
+    }
+
+    /**
+     * Standard IntelliJ generated hashCode.
+     * @return int hash
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername(), getScore(), getTimestamp());
+    }
+}
+
--- src/main/java/menu/LeaderBoardScreenController.java
+++ src/main/java/menu/LeaderBoardScreenController.java
@@ -1,19 +1,49 @@
 package menu;
 
+import database.Database;
+import game.Game;
+import game.LeaderBoardGame;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.ResourceBundle;
 
 /**
  * The type LoginScreen ViewController.
  */
-public class LeaderBoardScreenController {
+public class LeaderBoardScreenController implements Initializable {
 
     private Scene menuScreen;
 
+    private ArrayList<Game> top5Scores;
+
+    private transient ArrayList<LeaderBoardGame> leaderBoardGame = new ArrayList<>();
+
+    @FXML
+    private transient TableView<LeaderBoardGame> leaderBoard = new TableView<>();
+    @FXML
+    private transient TableColumn<LeaderBoardGame, String> usernameColumn;
+    @FXML
+    private transient TableColumn<LeaderBoardGame, Integer> scoreColumn;
+    @FXML
+    private transient TableColumn<LeaderBoardGame, Date> dateColumn;
+
+    private transient ArrayList<Game> top5;
+
+    private transient Database db = new Database();
     /**
      * Getter for Menu Screen scene.
      * @return menuScreen
@@ -50,5 +80,22 @@ public class LeaderBoardScreenController {
         Node node = (Node) event.getSource();
         Stage stage = (Stage) node.getScene().getWindow();
         stage.close();
+    }
+
+    @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        top5 = db.getTop5Scores();
+        for(Game game : top5) {
+            if(game.getAlias() == null)
+                leaderBoardGame.add(new LeaderBoardGame(game.getUsername(), game.getScore(), game.getTimestamp()));
+            else
+                leaderBoardGame.add(new LeaderBoardGame(game.getAlias(), game.getScore(), game.getTimestamp()));
+        }
+
+        usernameColumn.setCellValueFactory(new PropertyValueFactory<LeaderBoardGame, String>("username"));
+        scoreColumn.setCellValueFactory(new PropertyValueFactory<LeaderBoardGame, Integer>("score"));
+        dateColumn.setCellValueFactory(new PropertyValueFactory<LeaderBoardGame, Date>("date"));
+        ObservableList<LeaderBoardGame> top5Scores = FXCollections.observableArrayList(leaderBoardGame);
+        leaderBoard.setItems(top5Scores);
     }
 }
