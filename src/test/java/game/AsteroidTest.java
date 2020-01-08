6673a8a9c6d8b033eff0885f139b45ef41d96ce3
Pepijn te Marvelde <p.h.temarvelde@student.tudelft.nl>
2020-01-08 15:18:17 +0100

    Changed the SpaceEntity Move method to work like the "template" design pattern. Added tests for the extra methods added to the spaceEntity child classes.

--- src/test/java/game/AsteroidTest.java
+++ src/test/java/game/AsteroidTest.java
@@ -59,6 +59,7 @@ class AsteroidTest {
             if (s instanceof Large) {
                 lgCount++;
             }
+            s.checkMove();
         }
 
         System.out.println("Instances of Small: " + smCount);
